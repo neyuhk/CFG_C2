cdcf052a2ad7e1b598ee217cf590a659b37a3520
Neyuh <149485544+neyuhk@users.noreply.github.com>
2024-03-29 23:54:19 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-string check_sum(double maths, double literature, double english){
-    if(maths < 0 || maths > 10)
-        return "Input không hợp lệ.";
-
-    if(literature < 0 || literature > 10)
-        return "Input không hợp lệ.";
-
-    if(english < 0 || english > 10)
-        return "Input không hợp lệ.";
-
-    if(maths <= 1 || literature <= 1 || english <= 1)
-        return "Không đỗ trường Đại Học Công Nghệ.";
-
-    double sum = maths + literature + english;
-
-    if(sum >= 27.5)
-        return "Đỗ trường Đại Học Công Nghệ.";
-    else
-        return "Không đỗ trường Đại Học Công Nghệ.";
+string BMI(double weight, double height){
+    if(weight <= 0 || weight > 800 ||
+       height <= 0 || height > 3)
+        return "Input không hợp lệ";
+    double bmi = weight / (height * height);
+    if(bmi < 18.5)
+        return "Thiếu cân";
+    if(18.5 <= bmi && bmi <= 24.9)
+        return "Cân đối";
+    if(25.0 <= bmi && bmi <= 29.9)
+        return "Thừa cân";
+    return "Béo phì";
 }
 int main(){
     freopen("input.txt", "r", stdin);
@@ -26,9 +19,9 @@ int main(){
 
     int n;
     cin >> n;
-    double maths, literature, english;
+    double weight, height;
     while(n--){
-        cin >> maths >> literature >> english;
-        cout << check_sum(maths, literature, english) << endl;
+        cin >> weight >> height;
+        cout << BMI(weight, height) << endl;
     }
 }
